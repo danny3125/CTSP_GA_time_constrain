1f179ad86c31a037a38fd6173d7125dcb7172ac5
danny3125 <daniel0806789@gmail.com>
2022-05-15 13:37:33 +0800

    need to fix input

--- Individual.py
+++ Individual.py
@@ -79,7 +79,7 @@ class Individual:
         if self.fit == None: self.fit = self.__class__.fitFunc(self.x)
 
 
-MB_INFO = utility.MotherBoardInput('mother_board.png', '10&15data/25_chips/25_1.json').info_extraction()
+MB_INFO = utility.MotherBoardInput('mother_board.png', '10&15data/25_chips/25_2corners.json').info_extraction()
 RECT_LIST = MB_INFO[0]
 print(len(RECT_LIST))
 print(RECT_LIST)
